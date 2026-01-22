97b7b1bba71f58008a5ceda6d86dd172cd727025
jordiluque <67098497+jordiluque@users.noreply.github.com>
2026-01-22 11:57:12 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Telefonica-Scientific-Research"
+original_name="euvalues"
+original_urlname="EUValues"
+original_description="Awesome euvalues created by Telefonica-Scientific-Research"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv euvalues $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ euvalues/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for euvalues."""
+
+from euvalues.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
